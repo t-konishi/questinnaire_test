ddc32597fcd4dd4634603403ab350b2be0f2c256
t-konishi <konishi@mbd.ocn.ne.jp>
2018-05-26 16:27:07 +0900

    Devise Controller カスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # customized registrations and sessions by T.Konishi 2018/5/26
+  devise_for :users, :controllers => {
+   :registrations => 'users/registrations',
+   :sessions => 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'questionnaires#new'
   resources :questionnaires
